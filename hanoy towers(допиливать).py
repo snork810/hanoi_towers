2642f50824b398ab04e53e5e44396b5ba8a884d4
Виктор <110487403+snork810@users.noreply.github.com>
2024-02-12 21:51:18 +0300

    Update hanoy towers.py
    Почти финальная версия. Уже можно играть. Осталось допилить выход из игры при выйгрыше

--- hanoy towers(допиливать).py
+++ hanoy towers(допиливать).py
@@ -1,5 +1,10 @@
 import graphics as gr
+import tkinter as tr
+import pygame
 window = gr.GraphWin('HanoyTowers', 600, 600)
+
+running = True
+
 osnovanie = gr.Rectangle(gr.Point(100, 500), gr.Point(500, 600)).draw(window)
 osnovanie.setFill('black')
 stolb1 = gr.Line(gr.Point(150, 500), gr.Point(150, 200)).draw(window)
@@ -8,23 +13,85 @@ stolb3 = gr.Line(gr.Point(450, 500), gr.Point(450, 200)).draw(window)
 
 block1 = gr.Rectangle(gr.Point(100, 500), gr.Point(200, 480)).draw(window)
 block1.setFill('red')
+
 block2 = gr.Rectangle(gr.Point(125, 480), gr.Point(175, 460)).draw(window)
 block2.setFill('green')
 block3 = gr.Rectangle(gr.Point(140, 460), gr.Point(160, 440)).draw(window)
 block3.setFill('yellow')
 
- 
-while True:
+array_of_columns = [[block1,block2,block3], [], []]
+
+
+def block_color(block):
+    if sqware(block) == 2000.0:
+        return 'red'
+    elif sqware(block) == 1000.0:
+        return 'green'
+    elif sqware(block) == 400.0:
+        return 'yellow'
+
+
+def redraw_blocks(block, arr_out, arr_in):
+    if array_of_columns.index(arr_out) == 2 and array_of_columns.index(arr_in) == 0:
+        block.setFill('white')
+        block.move(-300, 0)
+        block.setFill(block_color(block))
+    elif array_of_columns.index(arr_out) > array_of_columns.index(arr_in):
+        block.setFill('white')
+        block.move(-150, 0)
+        block.setFill(block_color(block))
     
-    clickPoint = window.getMouse()
-    print(clickPoint)
-    if block2.getP1().getX() < clickPoint.getX() < block2.getP2().getX() and block2.getP1().getY() < clickPoint.getY() < block2.getP2().getY():
-        block2.move()
-    # # elif block2.getP1().getX() < clickPoint.getX() < block2.getP2().getX() and block2.getP1().getY() < clickPoint.getY() < block2.getP2().getY():
-    # #     block2.move(150, 0)
-    # # elif block1.getP1().getX() < clickPoint.getX() < block1.getP2().getX() and block1.getP1().getY() < clickPoint.getY() < block1.getP2().getY():
-    # #     block2.move(150, 0)
-    
+    elif array_of_columns.index(arr_out) == 0 and array_of_columns.index(arr_in) == 2:
+        block.setFill('white')
+        block.move(300, 0)
+        block.setFill(block_color(block))
+    elif array_of_columns.index(arr_out) < array_of_columns.index(arr_in):
+        block.setFill('white')
+        block.move(150, 0)
+        block.setFill(block_color(block))
+
+    pass
+
+arr1 = tr.PhotoImage(file = r"F:\PYTHON\pyton\hanoy_tower\2.png")
+arr2 =tr.PhotoImage(file = r"F:\PYTHON\pyton\hanoy_tower\98.jpg")
+
+button1 = tr.Button(window, image=arr1, command = lambda: Block_move(array_of_columns[0][-1], array_of_columns[0], array_of_columns[2])).place(x=100, y=200)
+button2 = tr.Button(window, image=arr2, command = lambda: Block_move(array_of_columns[0][-1], array_of_columns[0], array_of_columns[1])).place(x=160, y=200)
+button3 = tr.Button(window, image=arr1, command = lambda: Block_move(array_of_columns[1][-1], array_of_columns[1], array_of_columns[0])).place(x=250, y=200)
+button4 = tr.Button(window, image=arr2, command = lambda: Block_move(array_of_columns[1][-1], array_of_columns[1], array_of_columns[2])).place(x=310, y=200)
+button5 = tr.Button(window, image=arr1, command = lambda: Block_move(array_of_columns[2][-1], array_of_columns[2], array_of_columns[1])).place(x=400, y=200)
+button6 = tr.Button(window, image=arr2, command = lambda: Block_move(array_of_columns[2][-1], array_of_columns[2], array_of_columns[0])).place(x=460, y=200)
+
+
+def sqware(block):
+    return abs(block.p2.x-block.p1.x)*abs(block.p2.y-block.p1.y)
+
+
+def Block_move(block, s_out, s_in):
+    block_index_out = s_out.index(block)
+    if not s_out:
+        print('Нечего перекладывать!')
+    elif not s_in:
+        b = s_out[-1]
+        s_out.remove(b)
+        s_in.append(b)
+        print(f'Перекложили {b} иЗ {s_out} в {s_in}')
+        block_index_in = s_in.index(block)
+        redraw_blocks(block, s_out, s_in)
+    elif sqware(s_in[-1]) > sqware(s_out[-1]):
+        print('Переклали')
+        b = s_out[-1]
+        s_out.remove(b)
+        s_in.append(b)
+        print(f'Перекложили {b} из {s_out} в {s_in}')
+        block_index_in = s_in.index(block)
+        redraw_blocks(block, s_out, s_in)
+    else:
+        print('Так не пойдет!')
+    if len(s_in) == 3:
+        print('Победа!') 
+
+
 
 window.getMouse()
 window.close()
